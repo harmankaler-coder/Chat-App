9f6fcc4510d357dbfaf89e9b91295db3c7ab6b52
Harman Kaler <124948532+harmankaler-coder@users.noreply.github.com>
2025-03-02 01:12:03 +0530

    backend pull request

---
+++ frontend/lib/backend_services.dart
@@ -0,0 +1,16 @@
+import 'package:http/http.dart' as http;
+
+class BackendService {
+  static Future<String> sendCommand(String command) async {
+    final response = await http.post(
+      Uri.parse("http://localhost:5000/process"),
+      body: {"command": command},
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "Error: Unable to process command.";
+    }
+  }
+}
